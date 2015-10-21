e9ad5faa5b7bc0092416950def51851010ac1e0d
clonq <ovi.comes@gmail.com>
2015-10-20 21:52:33 -0400

    website skeleton

--- scripts/create-bucket.sh
+++ scripts/create-bucket.sh
@@ -1,1 +1,3 @@
-echo create-bucket not implemented
+aws s3 mb s3://io.r3v0
+aws s3 website s3://io.r3v0 --index-document index.html --error-document error.html
+aws s3api put-bucket-policy --bucket io.r3v0 --policy file://policies/website.json
--- scripts/update-website.sh
+++ scripts/update-website.sh
@@ -1,1 +1,1 @@
-echo update-website not implemented
+aws s3 sync web s3://io.r3v0/ --acl public-read
